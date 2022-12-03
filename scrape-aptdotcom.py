72b1cf1072eaedd80a75bee3dea969217e8e3509
Marty Sauser <msauser2@gmail.com>
2022-12-02 22:12:19 -0500

    Add multiple places and pagination

--- scrape-aptdotcom.py
+++ scrape-aptdotcom.py
@@ -1,19 +1,45 @@
 import requests
+import time
+import random
 from bs4 import BeautifulSoup
 
-where = "wabasha-county-mn"
 
-url = f"https://www.apartments.com/{where}/"
+def random_delay():
+    time.sleep(3+random.random()*5)
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"}
 
-html_text = requests.get(url, headers=headers).text
-soup = BeautifulSoup(html_text, "html.parser")
-placards = soup.find(id="placards")
-properties = placards.find_all("div", class_="property-info")
-for prop in properties:
-    address = prop.find("div", class_="property-address").get_text()
-    beds = prop.find("div", class_="bed-range").get_text()
-    price = prop.find("div", class_="price-range").get_text()
-    link = prop.find("a", class_="property-link").get("href")
-    print(f'"{where}","{address}","{beds}","{price}","{link}"')
+places = ["wabasha-county-mn", "rochester-mn"]
+for where in places:
+    page = 1
+    while True:
+        if page == 1:
+            url = f"https://www.apartments.com/{where}/"
+        else:
+            url = f"https://www.apartments.com/{where}/{page}/"
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"}
+        print("delaying...")
+        random_delay()
+        print(f"getting {where} page {page}")
+        html_text = requests.get(url, headers=headers).text
+        soup = BeautifulSoup(html_text, "html.parser")
+        placards = soup.find(id="placards")
+        properties = placards.find_all("div", class_="property-info")
+        for prop in properties:
+            address_div = prop.find("div", class_="property-address")
+            beds_div = prop.find("div", class_="bed-range")
+            price_div = prop.find("div", class_="price-range")
+            if address_div and beds_div and price_div:
+                address = address_div.get_text()
+                beds = beds_div.get_text()
+                price = price_div.get_text()
+                link = prop.find("a", class_="property-link").get("href")
+                print(f'"{where}","{address}","{beds}","{price}","{link}"')
+        page_range = soup.find("span", class_="pageRange")
+        if page_range:
+            last_page = int(page_range.get_text().split()[-1])
+        else:
+            last_page = 1
+        if page >= last_page:
+            break
+        else:
+            page += 1
